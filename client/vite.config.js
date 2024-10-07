fc2dd3b5d2de2adb6b5d8977de6e319dd5b8f7a0
Enkeym <julian1996@yandex.ru>
2024-10-07 11:46:11 +0300

    Попытка исправить порт

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,6 @@ config({ path: path.resolve(__dirname, '../.env') })
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4173, // Измените порт на 4173, чтобы соответствовать клиенту
-    host: '0.0.0.0' // Указываем хост для того, чтобы Vite был доступен извне
+    port: 5000
   }
 })
